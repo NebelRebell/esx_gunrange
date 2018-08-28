8e82839c58a36f934fc9b018141ede0840bfb9df
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-28 15:20:03 +0200

    de.lua
    completed

---
+++ locales/de.lua
@@ -0,0 +1,27 @@
+Locales['de'] = {
+	['gunrange'] 			= " Reichweite ",
+	['actionMessage'] 		= "Benutze ~INPUT_PICKUP~ um das Menu zu öffnen.",
+	['difficulty'] 			= " Schwierigkeit ",
+	['easy'] 				= " Leicht ",
+	['normal'] 				= " Normal ",
+	['hard'] 				= " Schwer ",
+	['harder'] 				= " Schwerer ",
+	['impossible'] 			= " Unmöglich ",
+	['targets'] 			= " Ziele ",
+	['points'] 				= " Punkte ",
+	['you_got']			 	= "Du hast erreicht ",
+	['wait_for_turn'] 		= "~o~ Warte bis Du dran bist!",
+	['point'] 				= " Punkte",
+
+	--Scores
+	['show_board'] 			= "Benutze ~INPUT_PICKUP~",
+	['last_10'] 			= "Letzten 10",
+	['name'] 				= "Name: ",
+	['difficulty_2'] 		= "Schwierigekeit: ",
+	['targets_2'] 			= "Ziele: ",
+	["points_2"] 			= "Punkte: ",
+}
+
+-- Translation by NebelRebell
+-- You need one for your script?
+-- Write me ;) 've a nice day!
